3adbc94784237cd3e2c0d0b4bb233604dd818e39
EdonJung <2donny@naver.com>
2021-07-17 02:03:29 +0900

    Calendar page

---
+++ client/src/pages/Calendar.tsx
@@ -0,0 +1,12 @@
+import SumIndicatorWrapper from '../components/anlytics/SumIndicatorWrapper';
+import PageTitle from '../components/PageTitle';
+
+export default function Calendar() {
+  return (
+    <div>
+      <PageTitle title="Calendar" />
+      <h1>Calendar</h1>
+      <SumIndicatorWrapper />
+    </div>
+  );
+}
