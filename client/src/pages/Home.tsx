84fde2eb17c150d162e82f091b6ef72ddb518e4f
EdonJung <2donny@naver.com>
2021-07-17 02:03:07 +0900

    Home page router

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,16 +1,55 @@
 import styled from 'styled-components';
+import PageTitle from '../components/PageTitle';
+import { Switch, Route, Link } from 'react-router-dom';
+import routes from '../routes';
+import Calendar from './Calendar';
+import Analytics from './Analytics';
 
 interface Props {
-  darkModeInput: [Boolean, () => void];
+  darkModeInput: [boolean, () => void];
 }
 
 export default function Home({ darkModeInput }: Props) {
   return (
-    <div>
-      <p>Home page 입니다.</p>
-      <button onClick={darkModeInput[1]}>mode 변경</button>
-    </div>
+    <>
+      <Container>
+        <PageTitle title="Home" />
+        <Navigation>
+          <button onClick={darkModeInput[1]}>mode 변경</button>
+          <Link to={routes.calendar}>
+            <p>캘린더</p>
+          </Link>
+          <Link to={routes.analytics}>
+            <p>차트</p>
+          </Link>
+        </Navigation>
+
+        {/* Router  */}
+        <Switch>
+          <Route path={routes.calendar}>
+            <Calendar />
+          </Route>
+          <Route path={routes.analytics}>
+            <Analytics />
+          </Route>
+        </Switch>
+      </Container>
+    </>
   );
 }
 
-const Container = styled.div``;
+const Container = styled.div`
+  background-image: ${(props) => props.theme.backgroundImage};
+  background-attachment: ${(props) => props.theme.backgroundAttachment};
+`;
+
+const Wrapper = styled.div``;
+
+const Navigation = styled.nav`
+  display: flex;
+  padding: 3.4375rem;
+  height: 2.9375rem;
+  width: calc(100% - 1.875rem);
+  justify-content: space-around;
+  align-items: center;
+`;
