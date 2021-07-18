bfd1711875ca72a66cdcee6e5c0c70ba57c46592
EdonJung <2donny@naver.com>
2021-07-18 13:41:53 +0900

    register click listener

--- client/src/components/anlytics/SumIndicatorWrapper.tsx
+++ client/src/components/anlytics/SumIndicatorWrapper.tsx
@@ -1,13 +1,67 @@
+import { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import { FetchHistoryData } from '../../lib/api/types';
+import { priceToString } from '../../lib/helper';
+import { SumIndicatorType } from '../../pages/HistoryView';
 
-interface Props {}
+interface Props {
+  historyData: FetchHistoryData[];
+  selectedSumIndicator: SumIndicatorType;
+  SumIndicatorClickedHandler: (data: SumIndicatorType) => void;
+}
 
-export default function SumIndicatorWrapper(props: Props) {
+interface MoneyType {
+  income: number;
+  expenditure: number;
+}
+
+export default function SumIndicatorWrapper({
+  historyData,
+  selectedSumIndicator,
+  SumIndicatorClickedHandler,
+}: Props) {
+  const [totalMoney, setTotalMoney] = useState<MoneyType>({
+    income: 0,
+    expenditure: 0,
+  });
+
+  useEffect(() => {
+    let income = 0;
+    let expenditure = 0;
+    historyData?.map((el) => {
+      if (el.type === 'INCOME' || el.type === 'RECEIVE') income += el.amount;
+      else expenditure += el.amount;
+    });
+    setTotalMoney({
+      income,
+      expenditure,
+    });
+  }, [historyData]);
+
   return (
     <Container>
       <SumIndicator>
-        <MoneyButtonIncome> +0 </MoneyButtonIncome>
-        <MoneyButtonExpenditure> -0 </MoneyButtonExpenditure>
+        <MoneyButtonIncome
+          active={selectedSumIndicator === 'INCOME'}
+          onClick={() =>
+            selectedSumIndicator === 'INCOME'
+              ? SumIndicatorClickedHandler(SumIndicatorType.DEFAULT)
+              : SumIndicatorClickedHandler(SumIndicatorType.INCOME)
+          }
+        >
+          {' '}
+          +{priceToString(totalMoney.income)}{' '}
+        </MoneyButtonIncome>
+        <MoneyButtonExpenditure
+          active={selectedSumIndicator === 'EXPENDITURE'}
+          onClick={() =>
+            selectedSumIndicator === 'EXPENDITURE'
+              ? SumIndicatorClickedHandler(SumIndicatorType.DEFAULT)
+              : SumIndicatorClickedHandler(SumIndicatorType.EXPENDITURE)
+          }
+        >
+          -{priceToString(totalMoney.expenditure)}
+        </MoneyButtonExpenditure>
       </SumIndicator>
     </Container>
   );
@@ -43,13 +97,16 @@ const MoneyButton = styled.div`
   cursor: pointer;
 `;
 
-const MoneyButtonIncome = styled(MoneyButton)`
+const MoneyButtonIncome = styled(MoneyButton)<{ active: boolean }>`
   margin-right: 0.9375rem;
-  border: 0.125rem solid blue;
-  color: blue;
+  border: 0.1rem solid #54aafc;
+  color: ${(props) => (props.active ? '#fff' : '#54aafc')};
+  background-color: ${(props) => (props.active ? '#54aafc' : 'transparent')};
 `;
 
-const MoneyButtonExpenditure = styled(MoneyButton)`
-  border: 0.125rem solid red;
-  color: red;
+const MoneyButtonExpenditure = styled(MoneyButton)<{ active: boolean }>`
+  border: 0.1rem solid #ee4337;
+  color: ${(props) => (props.active ? '#fff' : '#ee4337')};
+  background-color: ${(props) =>
+    props.active ? 'rgb(238, 67, 55)' : 'transparent'};
 `;
