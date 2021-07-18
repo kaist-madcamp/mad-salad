3ae9b774bb120bc1b3f0077175cf38f45694ed5a
EdonJung <2donny@naver.com>
2021-07-18 13:41:19 +0900

    chart done

---
+++ client/src/components/anlytics/CategoryChart.tsx
@@ -0,0 +1,220 @@
+import { fetchHistoryByCategoryAPI } from '../../lib/api/history';
+import styled, { keyframes } from 'styled-components';
+import { Pie } from 'react-chartjs-2';
+import { useQuery } from 'react-query';
+import { priceToString } from '../../lib/helper';
+import { FetchHistoryByCategoryData } from '../../lib/api/types';
+
+interface Props {
+  selectedDate: { year: number; month: number };
+}
+
+export default function CategoryChart({ selectedDate }: Props) {
+  const { data: historyData, isLoading, error } = useQuery<
+    FetchHistoryByCategoryData[] | undefined
+  >(
+    ['historyByCategory', selectedDate],
+    () => fetchHistoryByCategoryAPI(selectedDate),
+    {
+      retry: 1,
+    },
+  );
+
+  const dataForPie = {
+    labels: historyData?.map((el) => el.category),
+    datasets: [
+      {
+        data: historyData?.map((el) => el._sum.amount),
+        backgroundColor: historyData?.map((_, idx) => pieBackgroundColor[idx]),
+        borderWidth: 0,
+      },
+    ],
+  };
+
+  if (!historyData || historyData.length === 0)
+    return <Notification>No expenditures this month</Notification>;
+  if (isLoading) return <Notification>Loading...</Notification>;
+  if (error) return <Notification>Error..</Notification>;
+
+  const totalSum = historyData?.reduce((acc, el) => acc + el._sum.amount, 0);
+  return (
+    <>
+      <PolarCharWrapper>
+        <Pie
+          width={300}
+          height={300}
+          type={'pie'}
+          data={dataForPie}
+          options={{
+            maintainAspectRatio: false,
+          }}
+        />
+      </PolarCharWrapper>
+      <Container>
+        {historyData.map((el) => {
+          console.log(el._sum.amount);
+          return (
+            <Row key={el.category}>
+              <Column className="categoryName">{el.category}</Column>
+              <Column className="percent">{`${(
+                (el._sum.amount / totalSum) *
+                100
+              ).toFixed(2)}%`}</Column>
+              <Column className="bar">
+                <Bar
+                  width={`${((el._sum.amount / totalSum) * 100).toFixed(2)}%`}
+                />
+              </Column>
+              <Column className="amount">
+                {priceToString(el._sum.amount)}
+              </Column>
+            </Row>
+          );
+        })}
+      </Container>
+    </>
+  );
+}
+
+export const Notification = styled.span`
+  font-size: 1.25rem;
+  font-weight: 500;
+  display: block;
+  text-align: center;
+`;
+
+const slide = keyframes`
+    from {
+        transform: translateX(-100%);
+    }
+    to {
+        transform: translateX(0%);
+    }
+`;
+
+const Bar = styled.div<{ width: string }>`
+  background-color: #ff5959;
+  width: ${(props) => props.width};
+  height: 24px;
+  border-radius: 0.3125rem;
+  animation: ${slide} 0.5s ease forwards;
+  animation-delay: 0.1s;
+  -webkit-animation-delay: 0.1s;
+`;
+
+const Container = styled.div`
+  margin: 2rem 0;
+  padding: 0;
+  border: 0;
+  vertical-align: baseline;
+`;
+
+const Row = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  width: 100%;
+  background-color: ${(props) => props.theme.itemRowBgColor};
+  padding: 1.175rem;
+  border-radius: 0.625rem;
+  margin-bottom: 0.9375rem;
+  .categoryName {
+    color: #aaa;
+  }
+  .name {
+    width: 100%;
+    max-width: 4.5rem;
+    text-align: right;
+    font-weight: 500;
+    color: ${(props) => props.theme.itemRowColor};
+  }
+  .percent {
+    width: 100%;
+    max-width: 4.5em;
+    text-align: right;
+    margin-left: 0.625rem;
+    font-weight: 600;
+    font-size: 0.9em;
+  }
+  .bar {
+    width: 100%;
+    margin-left: 0.7em;
+    min-width: 15em;
+    border-radius: 0.3125rem;
+    overflow: hidden;
+  }
+  .amount {
+    color: rgb(238, 67, 55);
+    width: 100%;
+    max-width: 7em;
+    font-weight: 600;
+  }
+  &:nth-child(1) ${Bar} {
+    background: #ff5959;
+    animation-delay: 100ms;
+  }
+  &:nth-child(2) ${Bar} {
+    background: #f98f54;
+    animation-delay: 200ms;
+  }
+  &:nth-child(3) ${Bar} {
+    background: #f0c350;
+    animation-delay: 300ms;
+  }
+  &:nth-child(4) ${Bar} {
+    background: #7bca3d;
+    animation-delay: 400ms;
+  }
+  &:nth-child(5) ${Bar} {
+    background: #56db9b;
+    animation-delay: 500ms;
+  }
+  &:nth-child(6) ${Bar} {
+    background: #2bcfda;
+    animation-delay: 600ms;
+  }
+  &:nth-child(7) ${Bar} {
+    background: #5580ef;
+    animation-delay: 700ms;
+  }
+  &:nth-child(8) ${Bar} {
+    background: #9979f3;
+    animation-delay: 800ms;
+  }
+  &:nth-child(9) ${Bar} {
+    background: #d879f0;
+    animation-delay: 900ms;
+  }
+  &:nth-child(10) ${Bar} {
+    background: #ff5fa2;
+    animation-delay: 1000ms;
+  }
+`;
+
+const Column = styled.div`
+  width: 100%;
+  max-width: 4.5em;
+  text-align: right;
+  margin-left: 0.625rem;
+  font-weight: 600;
+  font-size: 0.9em;
+  color: #979797;
+`;
+
+const PolarCharWrapper = styled.div`
+  width: 100%;
+  margin: 0 0 3em;
+`;
+
+const pieBackgroundColor = [
+  '#FF5959',
+  '#F98F54',
+  '#F0C350',
+  '#7BCA3D',
+  '#56DB9B',
+  '#2BCFDA',
+  '#5580EF',
+  '#9979F3',
+  '#D879F0',
+  '#FF5FA2',
+];
---
+++ client/src/components/anlytics/DateChart.tsx
@@ -0,0 +1,78 @@
+import { useQuery } from 'react-query';
+import { Line } from 'react-chartjs-2';
+import styled from 'styled-components';
+import { fetchHistoryAPI } from '../../lib/api/history';
+import { Notification } from './CategoryChart';
+import { FetchHistoryData } from '../../lib/api/types';
+
+interface Props {
+  selectedDate: { year: number; month: number };
+}
+
+const DateChart = ({ selectedDate }: Props) => {
+  const { data: historyData, isLoading, error } = useQuery<
+    FetchHistoryData[] | undefined
+  >(['history', selectedDate], () => fetchHistoryAPI(selectedDate), {
+    retry: 1,
+  });
+
+  const dailyArray = [
+    ...Array(new Date(selectedDate.year, selectedDate.month, 0).getDate()),
+  ].map((_, i) => i + 1);
+
+  const zeroArr = new Array(dailyArray?.length).fill(0);
+
+  historyData?.map((el) => {
+    if (el.type === 'INCOME' || el.type === 'RECEIVE') return;
+    const idx = new Date(el.createdAt).getDate() - 1;
+    zeroArr[idx] += el.amount;
+  });
+
+  const datas = {
+    labels: dailyArray,
+    datasets: [
+      {
+        label: '이번달 지출',
+        data: zeroArr,
+        borderWidth: 2,
+        radius: 8,
+        borderColor: 'rgb(0, 145, 250)',
+        backgroundColor: 'rgb(0, 145, 250)',
+        pointBorderColor: '#fff',
+      },
+    ],
+  };
+
+  if (!historyData || historyData?.length === 0)
+    return <Notification>No expenditures this month</Notification>;
+
+  if (isLoading) return <Notification>Loading...</Notification>;
+  if (error) return <Notification>Error..</Notification>;
+  return (
+    <Container>
+      <Line type={'line'} height={180} data={datas} options={options} />
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  margin: 0 0 10rem;
+`;
+
+export default DateChart;
+
+const options: Chart.ChartOptions = {
+  scales: {
+    yAxes: [
+      {
+        ticks: {
+          beginAtZero: true,
+        },
+      },
+    ],
+  },
+  tooltips: {
+    titleFontColor: '#fff',
+    titleMarginBottom: 30,
+  },
+};
